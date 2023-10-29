0bdeb6f61d57e4ab85d91484d30733289736d715
ezagof_home <ezagof@gmail.com>
2023-10-29 15:11:14 +0100

    include the stdarg and start the structure of my ft_printf function, have to handle the va_arg

--- ft_printf.c
+++ ft_printf.c
@@ -11,6 +11,37 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdarg.h>
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		nb_printed_chars;
+	va_list	args;
+
+	i = -1;
+	nb_printed_chars = 0;
+	va_start(args, str);
+	while (str[++i])
+	{
+		/*create a funct° which add 1 to the nb_smthg and write the char*/
+		if (str[i] != '%')
+		{
+			write(1, &str[i], 1);
+			nb_printed_chars++;
+		}
+		if (str[i] == '%' && str[i + 1] == '%')
+		{
+			write(1, "%", 1);
+			nb_printed_chars++;
+		}
+		else
+		{
+			/*something appens here with an va_arg*/
+		}
+	}
+	return (nb_printed_chars);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -19,7 +50,10 @@ int	main(int argc, char *argv[])
 	{
 		letter = argv[1][0];
 		if (ft_isalpha(letter))
+		{
+			ft_printf(" ==%printf%%== ");
 			printf("%c est une lettre", letter);
+		}
 		else
 			printf("%c n'est pas une lettre", letter);
 	}
