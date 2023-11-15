7063ff58314856bdc943ec42177bea3aead06f69
ezagof_home <ezagof@gmail.com>
2023-11-16 00:26:40 +0100

    plan with comments the implementation of the ctype flag managment

--- conversions_b.c
+++ conversions_b.c
@@ -38,11 +38,20 @@ int	ft_ctype(t_list *lst, va_list vaarg, char *s)
 	}
 	else if (*s == '-' && '1' <= *(s + 1) && *(s + 1) <= '9')
 	{
+		// FAIRE MANIP DE STRLCAT ET LSTADD DANS UNE FONCTION A PART (utile pr ts les flags)
+		// cat_lstadd, cut_lstadd !!! ATTENTON AUX FREES!!!
 		// work from there
+		// atoi renvoie un nombre qui defini le nbr de ' ' avant le char
+		// faire cette commande mais avec le strlcat de atoi-1 space puis le vaarg : 
+		// ft_lstadd_back(&lst, ft_lstnew(ft_c_to_str(va_arg(vaarg, int), 1)));
 	}
 	else if ('1' <= *s && *s <= '9')
 	{
+		// FAIRE MANIP DE STRLCAT ET LSTADD DANS UNE FONCTION A PART (utile pr ts les flags)
 		// work from there
+		// atoi renvoie un nombre qui defini le nbr de ' ' apres le char
+		// faire cette commande mais avec le strlcat de vaarg puis atoi-1 space : 
+		// ft_lstadd_back(&lst, ft_lstnew(ft_c_to_str(va_arg(vaarg, int), 1)));
 	}
 	return (0);
 }
